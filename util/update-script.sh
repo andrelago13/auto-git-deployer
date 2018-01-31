79de1a6ef011cd4f3b0b7f124770f80e119a6f86
André Lago <andrelago1995@gmail.com>
2018-01-31 16:38:09 +0000

    Minor fixes

--- util/update-script.sh
+++ util/update-script.sh
@@ -2,32 +2,37 @@
 mkdir /jarvis-temp
 
 # First, get the zip file
-cd /jarvis-temp && wget -O projectmaster.zip -q https://github.com/andrelago13/jarvis.git
+git clone https://github.com/andrelago13/jarvis.git /jarvis-temp
 
 # Second, unzip it, if the zip file exists
-if [ -f /jarvis-temp/projectmaster.zip ]; then
-    # Unzip the zip file
-    unzip -q /jarvis-temp/projectmaster.zip
-
-    # Delete zip file
-    rm /jarvis-temp/projectmaster.zip
+if [ -d /jarvis-temp ]; then
+    #Bring docker down
+    (cd /jarvis && docker-compose down)
+    (cd /jarvis && docker-compose rm)
 
     # Delete previous dir
     rm -rf /jarvis
+    mkdir /jarvis
 
     # Rename project directory to desired name
-    mv -f website-master app
+    mv -f /jarvis-temp/* /jarvis
+
+    #Restart docker
+    (cd /jarvis && docker-compose up --build -d)
+
+    #Remove temp dir
+    rm -rf /jarvis-temp
 
     # Delete current directory
-    rm -rf /www/website-enei/app
+    # rm -rf /www/website-enei/app
 
     # Replace with new files
-    cp -rf /www/website-enei/temp-zip/app /www/website-enei/app
+   #cp -rf /www/website-enei/temp-zip/app /www/website-enei/app
 
-    (cd /www/website-enei/app/app && npm install && pm2 reload app)
+    #(cd /www/website-enei/app/app && npm install && pm2 reload app)
 
     # Delete unziped files
-    rm -rf /www/website-enei/temp-zip/website-master
+    #rm -rf /www/website-enei/temp-zip/website-master
 
 
     # Perhaps call any other scripts you need to rebuild assets here
